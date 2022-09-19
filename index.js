45602cb8b5e8e8529368c898e7abf75499a08eac
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-19 10:52:22 +1000

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+//use find() to isolate a specific result
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+function superbowlWin(element) {
+    if (element.result === "W") {
+        console.log("this is a win");
+        console.log(element.year);
+        return element.year;
+    }
+    //return undefined if no win is found, so no return
+}
+
+console.log(record.find(superbowlWin));
